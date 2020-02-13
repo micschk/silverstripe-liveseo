2ad8e77bc7cc256ab9ca373c9f0b8afea1ce0aab
Morven Lewis-Everley <morven@i-lateral.com>
2020-02-13 16:19:10 +0000

    Merge additional extension code

--- src/SeoSiteConfig.php
+++ src/SeoSiteConfig.php
@@ -2,9 +2,15 @@
 
 namespace Restruct\Silverstripe\LiveSEO;
 
-use DataExtension;
-use FieldList;
-use TextareaField;
+use SilverStripe\View\ArrayData;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\Forms\TextareaField;
+use Wilr\GoogleSitemaps\GoogleSitemap;
+use SilverStripe\Core\Manifest\ModuleLoader;
 
 /**
  * SeoSiteConfig
@@ -13,20 +19,52 @@ use TextareaField;
 class SeoSiteConfig extends DataExtension {
 
 	private static $db = array(
+        'SEOTitleTemplate' => 'Varchar(255)',
 		'GoogleWebmasterMetaTag' => 'Varchar(512)'
 	);  
-
 
 	/**
 	 * updateCMSFields.
  	 * Update Silverstripe CMS Fields for SEO Module
- 	 *  	 
+ 	 *
 	 * @param FieldList
 	 * @return none
 	 */
 	public function updateCMSFields(FieldList $fields) {
+		$manifest = ModuleLoader::inst()->getManifest();
+		$GSMactive = false;
+		$GSMping = false;
+		$RedirActive = false;
 
-        $fields->addFieldToTab("Root.SEO",
+		// check for Google Sitemaps module & notification;
+		if ($manifest->moduleExists('wilr/silverstripe-googlesitemaps')) {
+			$GSMactive = Config::inst()->get(GoogleSitemap::class, 'enabled');
+			$GSMping = Config::inst()->get(GoogleSitemap::class, 'google_notification_enabled');
+		}
+
+		// check if Redirectmanager and CMS are installed
+		if ($manifest->moduleExists('silverstripe/cms') && $manifest->moduleExists('silverstripe/redirectedurls')) {
+			$RedirActive = true;
+		}
+
+		$fields->addFieldToTab(
+			"Root.SEO",
+			LiteralField::create(
+				'SEOtips',
+				$this->owner->customise(
+					ArrayData::create(
+						array(
+							'GSMactive' => $GSMactive,
+							'GSMping' => $GSMping,
+							'RedirActive' => $RedirActive
+						)
+					)
+				)->renderWith('AdminSiteConfigSeoTips')
+			)
+		);
+
+        $fields->addFieldToTab(
+			"Root.SEO",
             TextareaField::create("GoogleWebmasterMetaTag", _t('SEO.SEOGoogleWebmasterMetaTag', 'Google webmaster meta tag'))
             ->setRightTitle(
                 _t(
@@ -34,9 +72,21 @@ class SeoSiteConfig extends DataExtension {
                     "Full Google webmaster meta tag For example &lt;meta name=\"google-site-verification\" content=\"hjhjhJHG12736JHGdfsdf\" /&gt;"
                 )
             )
-        );
+		);
 
+        $fields->addFieldToTab(
+			"Root.SEO",
+			$seotitlefield = TextField::create('SEOTitleTemplate')
+                ->setRightTitle(
+					"For SEO preview (valid js expression, available vars: page_title, page_menutitle, '.
+					'page_metadata_title), eg:<br /> page_title + ' &raquo; ' +
+					siteconfig_title [OR] (page_metadata_title ? page_metadata_title : page_title)"
+				)
+		);
+
+        // set default/initial value
+        if (!$this->owner->SEOTitleTemplate) {
+            $seotitlefield->setValue("page_title + ' &raquo; ' + siteconfig_title");
+		}
     }
-
-
 }
